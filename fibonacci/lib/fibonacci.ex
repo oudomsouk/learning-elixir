94ad9a3447c343ee78ef7826402450dc95931385
Dillon Oudomsouk <dillon.oudomsouk@tutanota.com>
2020-06-06 12:51:49 -0400

    calculate fibonacci with agent

---
+++ fibonacci/lib/fibonacci.ex
@@ -0,0 +1,17 @@
+defmodule Fibonacci do
+
+  alias Fibonacci.Cache
+
+  def fib(n) do
+    Cache.run(fn cache ->
+      cached_fib(n, cache)
+    end)
+  end
+
+  defp cached_fib(n, cache) do
+    Cache.lookup(cache, n, fn ->
+      cached_fib(n-2, cache) + cached_fib(n-1, cache)
+    end)
+  end
+
+end
