dfac3da04553e9f8eb102d0d9f4b1c742a20c76d
Dillon Oudomsouk <dillonoudomsouk@carfax.com>
2020-08-28 15:55:04 -0400

    add full hangman functionality

--- gallows/lib/gallows_web/controllers/hangman_controller.ex
+++ gallows/lib/gallows_web/controllers/hangman_controller.ex
@@ -13,4 +13,14 @@ defmodule GallowsWeb.HangmanController do
     |> render("game_field.html", tally: tally)
   end
 
+  def make_move(conn, params) do
+    guess = params["make_move"]["guess"]
+    tally =
+      conn
+      |> get_session(:game)
+      |> Hangman.make_move(guess)
+    put_in(conn.params["make_move"]["guess"], "")
+    |> render("game_field.html", tally: tally)
+  end
+
 end
--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,6 +1,16 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
+  import Gallows.Views.Gelpers.GameStateHelper
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
   def format_letters(letters) do
     letters
     |> Enum.join(" ")
